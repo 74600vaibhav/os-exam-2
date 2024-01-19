36fbff13338f0e9bbfa4bf05e98bcd4b472f863a
vaibhav narkhede <vnarkhede.1819@gmail.com>
2024-01-19 09:27:27 +0530

    Initial commit

---
+++ factorial/src/factorial/fact.java
@@ -0,0 +1,18 @@
+package factorial;
+
+public class fact {
+	
+	public static void main(String[] args) {
+		int num ,fact,i;
+		fact = 1;
+		num =5;
+		
+	for(i=2 ; i<=num ; i++)
+		{
+			fact=fact*i;	
+		}
+	System.out.println("\n fact of is "+fact);
+		// TODO Auto-generated method stub
+
+	}
+}
